8e1bfe54a863b3c878053846e843758030d53b20
chipo <pnapoli33@gmail.com>
2020-10-08 17:57:01 -0300

    Add ApplicationSecurityConfig where in overriden configure(HttpSecurity http) method is defined that any request must be authenticated and the use of httpBasic.

---
+++ src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.binary_winters.spring_security.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
