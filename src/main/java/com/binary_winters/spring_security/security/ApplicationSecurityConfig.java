ae9e43ca0b56948d2c144c4858defa147d67935f
chipo <pnapoli33@gmail.com>
2020-10-08 18:15:56 -0300

    Instead of using the default user with its random password generated by Spring boot, I define my own user through the overriden userDetailsService() method; I create the bean passwordEncoder in PasswordConfig configuration class to use it to encode the password from the user and so Basic Auth allows me to use the user.

--- src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
@@ -1,15 +1,30 @@
 package com.binary_winters.spring_security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	// The passwordEncoder input parameter is the object generated in PasswordConfig class.
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
 
-    @Override
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
@@ -19,6 +34,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+    
+    @Override
+    @Bean
+    // This method indicates how I do retrieve the user from the db.
+    protected UserDetailsService userDetailsService() {
+    	UserDetails lucaUser = User.builder()
+    		.username("luca")
+    		.password(passwordEncoder.encode("luca123"))
+    		.roles("STUDENT") // ROLE_STUDENT is how Spring Security understands it.
+    		.build();
+    	
+    	return new InMemoryUserDetailsManager(lucaUser);
     }
 
 }
