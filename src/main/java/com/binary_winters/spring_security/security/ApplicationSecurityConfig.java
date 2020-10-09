827f18aa24adcb0a62db1530444e8da7dd7cae2b
chipo <pnapoli33@gmail.com>
2020-10-09 00:28:46 -0300

    Invoke http.csrf().disable() from configure(HttpSecurity http) in order to be able to use methods from controller which contains a body; Add antMatchers to link the type of request, the URL and the role or permission; Add authorities to users because having only .roles they weren't aware about the permissions.

--- src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.binary_winters.spring_security.security;
 
 import static com.binary_winters.spring_security.security.ApplicationUserRole.*;
+import static com.binary_winters.spring_security.security.ApplicationUserPermission.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,9 +31,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+        		.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name()) // The order matters here cause I'm not specifying the type of request.
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -42,22 +49,40 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     // This method indicates how I do retrieve the user from the db.
     protected UserDetailsService userDetailsService() {
-    	UserDetails lucaUser = User.builder()
+    	/*
+    	 * In UserDetails there is no concept of role or permissions and that is bundle inside the collection 
+    	 * getAuthorities which returns any type wich extends GrantedAuthority.
+
+		 * A User (from Spring of type UserDetails) has a list of Roles of type GrantedAuthority and each of 
+		 * its roles is an implementation of that interface of type SimpleGrantedAuthority (public UserBuilder 
+		 * roles(String... roles) method).
+    	 */
+    	UserDetails annaSmithUser = User.builder()
     		.username("annasmith")
     		.password(passwordEncoder.encode("annasmith123"))
-			.roles(STUDENT.name()) // ROLE_STUDENT
+//			.roles(STUDENT.name()) // ROLE_STUDENT
+    		.authorities(STUDENT.getGrantedAuthorities())
     		.build();
     	
 		UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("linda123"))
-                .roles(ADMIN.name()) // ROLE_ADMIN
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("tom123"))
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
-                lucaUser,
-                lindaUser
-        ); 
+                annaSmithUser,
+                lindaUser,
+                tomUser
+        );
     	 
     }
 
--- src/main/java/com/binary_winters/spring_security/security/ApplicationUserRole.java
+++ src/main/java/com/binary_winters/spring_security/security/ApplicationUserRole.java
@@ -1,12 +1,23 @@
 package com.binary_winters.spring_security.security;
 
+import static com.binary_winters.spring_security.security.ApplicationUserPermission.COURSE_READ;
+import static com.binary_winters.spring_security.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.binary_winters.spring_security.security.ApplicationUserPermission.STUDENT_READ;
+import static com.binary_winters.spring_security.security.ApplicationUserPermission.STUDENT_WRITE;
+
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import com.google.common.collect.Sets;
-import static com.binary_winters.spring_security.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
     private final Set<ApplicationUserPermission> permissions;
 
@@ -15,6 +26,18 @@ public enum ApplicationUserRole {
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    // I'm adding a role to the list of permissions.
+    // Due to UserDetails has this property: Collection<? extends GrantedAuthority> getAuthorities();
+    // this method will be useful to set the list returned for it to the User.
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
         return permissions;
     }
 }
