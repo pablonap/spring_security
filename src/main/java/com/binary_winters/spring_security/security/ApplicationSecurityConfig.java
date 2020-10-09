0ab2a00b4ec0357474465a15b89511f47a15bffa
chipo <pnapoli33@gmail.com>
2020-10-09 02:17:52 -0300

    Disable CSRF token generation in order to continue working more easily with the app.

--- src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -34,8 +33,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+        		.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
