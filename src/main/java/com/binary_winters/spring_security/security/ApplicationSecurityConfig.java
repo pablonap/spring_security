0bf27f78d6092a4f8b29d0a306a24387f238435b
chipo <pnapoli33@gmail.com>
2020-10-09 15:44:09 -0300

    Add rememberMe() in configure method.

--- src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
@@ -42,7 +42,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
-				.defaultSuccessUrl("/courses", true);
+				.defaultSuccessUrl("/courses", true)
+				.and()
+				.rememberMe(); // default to 2 weeks
     }
     
     @Override
