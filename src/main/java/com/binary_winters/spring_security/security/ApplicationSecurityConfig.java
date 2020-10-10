71fe62c0a4e931c4bf7475ef1aa715b795f8c5cb
chipo <pnapoli33@gmail.com>
2020-10-10 12:37:02 -0300

    Add configuration to Logout.

--- src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/binary_winters/spring_security/security/ApplicationSecurityConfig.java
@@ -48,7 +48,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.rememberMe()
 					.tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-					.key("something_very_secure");
+					.key("something_very_secure")
+        		.and()
+        		.logout()
+        			.logoutUrl("/logout")
+        			.clearAuthentication(true)
+        			.invalidateHttpSession(true)
+        			.deleteCookies("JSESSIONID","remember-me")
+        			.logoutSuccessUrl("/login");
     }
     
     @Override
